fb43015852bd5fc050bb07b2c7f1372889635ad0
sostenesg7 <sostenesg7@gmail.com>
2021-09-13 22:21:42 -0300

    feat: Add database connection and data insertion

--- consumer.js
+++ consumer.js
@@ -1,4 +1,84 @@
 /**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+
+const saveToDatabase = async (message) => {
+  const {
+    USER,
+    SOCKET_PATH,
+    PASSWORD,
+    DATABASE,
+    HOST,
+  } = process.env;
+
+  const mysql = require('mysql');
+
+  console.log('MESSAGE', message)
+
+  const connection = mysql.createConnection({
+    host: HOST,
+    socketPath: SOCKET_PATH,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE,
+  });
+
+  try {
+    connection.connect();
+    console.log('DATABASE CONNECTED', message);
+
+    // connection.query({ sql: 'SELECT * FROM user_message', timeout: 40000, }, (err, res) => {
+    //   if (err) {
+    //     connection.end();
+    //     return console.error('SELECT', err)
+    //   }
+    //   connection.end();
+    //   console.log(res);
+    // })
+
+    connection.query(
+      'INSERT INTO user_message SET ?',
+      { text: message },
+      (error, results) => {
+        connection.end();
+        if (error) {
+          console.error('INSERT ERROR', error);
+          return;
+        }
+        console.log('MESSAGE SAVED TO DATABASE', results);
+      }
+    );
+  } catch (error) {
+    console.error('CONNECTION ERROR', error);
+    connection.end();
+  } finally {
+    // console.log('DATABASE CONNECTION CLOSED');
+  }
+};
+
+// Imports the Google Cloud client library
+exports.server = async (req, res) => {
+  const { PubSub } = require('@google-cloud/pubsub');
+
+  const message = req.query.message || req.body.message || 'Hello World!';
+  saveToDatabase(message);
+  // Instantiates a client
+  const pubsub = new PubSub();
+  // Send a message to the topic
+  try {
+    await pubsub.topic('taiguara-pub').publish(Buffer.from(message));
+    res.status(200).send(message);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+
+
+/**
  * Triggered from a message on a Cloud Pub/Sub topic.
  *
  * @param {!Object} event Event payload.
@@ -11,6 +91,7 @@ exports.consumer = (event, context) => {
   console.log('ORIGINAL MESSAGE', message);
 
   const { Translate } = require('@google-cloud/translate').v2;
+
   const translator = new Translate();
 
   (async () => {
